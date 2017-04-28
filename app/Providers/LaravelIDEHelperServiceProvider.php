68e9f8d363819c615c7ebcf4663f3d1693c60d42
AkKeRise <nguyenthanh.rise.88@gmail.com>
2017-04-28 14:59:49 +0700

    Rename file LaravelIDEHelperServiceProvider

--- app/Providers/LaravelIDEHelperServiceProvider.php
+++ app/Providers/LaravelIDEHelperServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelIDEHelperProvider extends ServiceProvider
+class LaravelIDEHelperServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
