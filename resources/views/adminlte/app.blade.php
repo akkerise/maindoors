1a30c3cedf12d8b3d8660f42cd0e8e68f1e84488
AkKeRise <nguyenthanh.rise.88@gmail.com>
2017-04-28 15:29:12 +0700

    Add secure_asset()

--- resources/views/adminlte/app.blade.php
+++ resources/views/adminlte/app.blade.php
@@ -6,19 +6,29 @@
   <title>AdminLTE 2 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ asset('adminlte/bootstrap/css/bootstrap.min.css') }}">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="{{ asset('adminlte/bootstrap/css/bootstrap.min.css') }}">
   <!-- jvectormap -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/AdminLTE.min.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{ asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
+
+  <link rel="stylesheet" href="{{ secure_asset('adminlte/bootstrap/css/bootstrap.min.css') }}">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{ secure_asset('adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ secure_asset('adminlte/dist/css/AdminLTE.min.css') }}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{ secure_asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -73,5 +83,32 @@
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('adminlte/dist/js/demo.js') }}"></script>
 <script src="{{ asset('adminlte/dist/js/adminlte.js') }}"></script>
+
+
+
+
+{{--Secure Asset--}}
+<!-- jQuery 2.2.3 -->
+<script src="{{ secure_asset('adminlte/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ secure_asset('adminlte/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ secure_asset('adminlte/plugins/fastclick/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ secure_asset('adminlte/dist/js/app.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ secure_asset('adminlte/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{ secure_asset('adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ secure_asset('adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- SlimScroll 1.3.0 -->
+<script src="{{ secure_asset('adminlte/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<!-- ChartJS 1.0.1 -->
+<script src="{{ secure_asset('adminlte/plugins/chartjs/Chart.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ secure_asset('adminlte/dist/js/pages/dashboard2.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ secure_asset('adminlte/dist/js/demo.js') }}"></script>
+<script src="{{ secure_asset('adminlte/dist/js/adminlte.js') }}"></script>
 </body>
 </html>
